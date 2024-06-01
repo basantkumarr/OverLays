30a06f321a0de038f7b2ea5b6a0b87a537048c60
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-01 23:42:57 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,12 +13,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-origin:"https://overlays-xi.vercel.app",
-  optionsSuccessStatus: 200,  
-  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+  origin: "https://overlays-xi.vercel.app",
+  optionsSuccessStatus: 200,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
   preflightContinue: false,
 }));
+
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
