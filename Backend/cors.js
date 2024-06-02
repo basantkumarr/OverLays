87fe1d9e30afc49b55b43e247b737e7ecdaf30f0
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-06-02 09:43:11 +0530

    Update cors.js

--- Backend/cors.js
+++ Backend/cors.js
@@ -1,1 +1,12 @@
 
+module.exports = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://overlays-xi.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+};
